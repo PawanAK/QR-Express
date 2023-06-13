7049aed3a3d9b387cb8544401d6909289a95e565
PawanAK <pawanajjark@gmail.com>
2023-06-13 22:52:01 +0530

    Setup 1 : Working fine , need to add Download button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./app.css";
+import QRCode from "react-qr-code";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  const generateQrcodeHandler = () => {
+    if (!value) return;
+    setVisible(true);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1>QR code Generator</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Type your content ..."
+      />
+      <button onClick={generateQrcodeHandler}>Generate</button>
+      {visible && (
+        <div className="qr-code-container">
+          <QRCode value={value} size={300} />
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
